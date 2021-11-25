0078a322b42b00af71524653d442ecafe6e195db
Lukis02 <74176312+Lukis02@users.noreply.github.com>
2021-11-25 21:58:02 +0100

    carrera.js

--- carrera.js
+++ carrera.js
@@ -3,5 +3,10 @@ car = [
         new car('Lambourghini','jose','abc1234',350),
         new car('Delorean','Marty','OUTATTIME',178),
         new car('Bugatti','Juan','cfe3546',407) ]
-        
+        circuit = new circuit("race1")
+
+        start(){
+                
+        console.log('la carrera ha terminado")
+                    
 }
