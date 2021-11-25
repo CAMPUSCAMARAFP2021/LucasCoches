3a12f08abdfcbb98de702d446f66ecb454e97d97
Lukis02 <74176312+Lukis02@users.noreply.github.com>
2021-11-25 22:35:21 +0100

    carrera.js

--- carrera.js
+++ carrera.js
@@ -4,9 +4,22 @@ car = [
         new car('Delorean','Marty','OUTATTIME',178),
         new car('Bugatti','Juan','cfe3546',407) ]
         circuit = new circuit("race1")
-
+        distance;
+        instant = 0;
+        TempResults = [];
+        Results = [];
+        
+        constructor(length, lap) {
+        this.length = length;
+        this.laps = lap;
+        }
+        winner(car) {
+        this.results.push(car)
+    }
         start(){
-                
+               
+        console.log('winner')        
         console.log('la carrera ha terminado")
+                    }
                     
 }
